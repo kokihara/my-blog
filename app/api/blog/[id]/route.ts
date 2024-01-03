48a86b985f04bb703f2e6f23fd111107624c57f9
kokihara <nov17hk@gmail.com>
2024-01-04 00:19:27 +0900

    詳細記事取得API開発

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { main } from "../route";
+
+const prisma = new PrismaClient();
+
+// ブログの詳細記事取得API
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    await main();
+    const post = await prisma.post.findFirst({ where: { id } });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { error } from "console";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
